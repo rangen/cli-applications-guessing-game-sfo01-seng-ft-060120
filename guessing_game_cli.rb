7dd7ce901f267ffc03d01709625022e6246899d9
Don Mallory <dmm333@gmail.com>
2020-02-23 21:41:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,10 @@
+require 'pry'
 def run_guessing_game
   secret_number = rand(6) + 1
 
   #puts "Please guess a number between 1 and 6"
   guess = gets.chomp
-
+binding.pry
   if guess == secret_number
     puts "You guessed the correct number!"
   elsif guess == "exit"
