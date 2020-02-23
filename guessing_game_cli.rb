69df704a333d1bbf50cfc7a90b91543531a1b282
Don Mallory <dmm333@gmail.com>
2020-02-23 21:38:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   secret_number = rand(1..6)
 
   puts "Please guess a number between 1 and 6"
-  guess = gets.chomp
+  guess = gets.chomp.to_i
 
   if guess == secret_number
     puts "You guessed the correct number!"
